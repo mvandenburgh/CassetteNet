c3d93dfe8fb9576fa4b9fbed0a966b93003e17d0
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-10-25 17:33:55 -0400

    Added rounded corners and shadows to the rows

--- client/src/components/pages/DashboardPage.jsx
+++ client/src/components/pages/DashboardPage.jsx
@@ -13,41 +13,69 @@ function DashboardPage(props) {
         mixtapeRows: '#1c108f'
     }
 
+    const containerBorderProps = {
+        bgcolor: 'background.paper',
+        borderColor: blueGrey[800],
+        m: 1,
+        border: 1,
+        boxShadow: 6,
+      };
+
+      const rowBorderProps = {
+        bgcolor: 'background.paper',
+        borderColor: '#1c108f',
+        m: 1,
+        border: 1,
+        boxShadow: 6,
+      };
+
     const useStyles = makeStyles((theme) => ({
-        grid: {
-            width: '100%',
-            margin: '25px'
-        },
-        list: {
-            width: '100%',
-            maxWidth: 360,
-            backgroundColor: theme.palette.background.paper,
-            position: 'relative',
-            overflow: 'auto',
-            maxHeight: 300,
-        },
-        card: {
-            padding: theme.spacing(2),
-            textAlign: 'center',
-            color:  theme.palette.text.secondary,
-            background: theme.palette.success.light,
-        },
         box_container: {
             padding: theme.spacing(2),
-            margin: '20px',
+            marginLeft: '100px',
+            marginRight: '100px',
+            marginTop: '50px',
+            marginBottom: '50px',
             textAlign: 'center',
             color:  theme.palette.info.contrastText,
             background: blueGrey[800],
-            maxHeight: 400, 
+            maxHeight: 300, 
             overflow: 'auto'
         },
         box_row: {
             padding: theme.spacing(2),
             margin: '10px',
             textAlign: 'center',
+            //display: 'inline',
             color:  'white',//theme.palette.text.secondary,
             background: colors.mixtapeRows,
-        }
+        },
+        title_row: {
+            padding: theme.spacing(2),
+            margin: '10px',
+            color:  'white',//theme.palette.text.secondary,
+            background: colors.mixtapeRows,
+            height: '50%',
+            
+        },
+        name_col: {
+            margin: '10px',
+            textAlign: 'left',
+            color:  'white',//theme.palette.text.secondary,
+            background: colors.mixtapeRows,
+        },
+        collaborators_col: {
+            margin: '10px',
+            textAlign: 'center',
+            color:  'white',//theme.palette.text.secondary,
+            background: colors.mixtapeRows,
+        },
+        favorites_col: {
+            margin: '10px',
+            textAlign: 'right',
+            color:  'white',//theme.palette.text.secondary,
+            background: colors.mixtapeRows,
+        },
     }));
 
     const classes = useStyles();
@@ -56,20 +84,28 @@ function DashboardPage(props) {
 
     return (
         <div style={{margin: '20px', background: 'transparent', height: '100%', width: '100%'}}>
-            <Box id='popular' className={classes.box_container}> 
-                <Typography variant="headline" component="h1">
-                    Popular Mixtapes This Week
-                </Typography>
-                <Box className={classes.box_row}> 
-                    blocc 3
+            <Box id='popular' className={classes.box_container} borderRadius={16} {...containerBorderProps}> 
+                <Typography variant="headline" component="h1">Popular Mixtapes This Week</Typography>
+                <Box className={classes.title_row} borderRadius={16} {...rowBorderProps}> 
+                    <Typography variant="headline" component="h2"  align="left">
+                        Name
+                    </Typography>
+                    <Typography variant="headline" component="h2"  align="center">
+                        Collaborators
+                    </Typography>
+                    <Typography variant="headline" component="h2" align="right">
+                        Favorites
+                    </Typography>
                 </Box>
-                <Box className={classes.box_row}> 
-                    blocc 1
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
+                    <Box className={classes.name_col}>summertime</Box>
+                    <Box className={classes.collaborators_col}>DrizzyD</Box>
+                    <Box className={classes.favorites_col}>109K</Box>
                 </Box>
-                <Box className={classes.box_row}> 
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
                     blocc 2
                 </Box>
-                <Box className={classes.box_row}> 
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
                     blocc A
                 </Box>
                 <Box className={classes.box_row}> 
@@ -79,17 +115,17 @@ function DashboardPage(props) {
                     blocc C
                 </Box>
             </Box>
-            <Box id='follower_activity' className={classes.box_container}> 
+            <Box id='follower_activity' className={classes.box_container} borderRadius={16} {...containerBorderProps}> 
                 <Typography variant="headline" component="h1">
                     Followed User Activity
                 </Typography>
-                <Box className={classes.box_row}> 
-                    blocc 4
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
+                    DrizzyD favorited a mixtape: summertime
                 </Box>
-                <Box className={classes.box_row}> 
-                    blocc 5
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
+                    bob created a new mixtape: bob's hits
                 </Box>
-                <Box className={classes.box_row}> 
+                <Box className={classes.box_row} borderRadius={16} {...rowBorderProps}> 
                     blocc 6
                 </Box>
                 <Box className={classes.box_row}> 
