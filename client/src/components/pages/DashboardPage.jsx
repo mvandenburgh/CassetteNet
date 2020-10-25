4f860af7bfabe1dfac819dd6bc68cdbfe92ac80b
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-10-24 20:37:29 -0400

    Added some containers to hold mixtapes on the dashboard

--- client/src/components/pages/DashboardPage.jsx
+++ client/src/components/pages/DashboardPage.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Button, Grid, Card, Paper, Box, Typography } from '@material-ui/core';
+import indigo from '@material-ui/core/colors/indigo';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 import { DataGrid } from '@material-ui/data-grid';
 import logo from '../../images/logo.png';
+import { makeStyles } from "@material-ui/core/styles";
 import UserContext from '../../contexts/UserContext';
 
 function DashboardPage(props) {
@@ -9,25 +12,63 @@ function DashboardPage(props) {
         buttonContainer: '#0A1941',
         mixtapeRows: '#1c108f'
     }
+
+    const useStyles = makeStyles((theme) => ({
+        grid: {
+            width: '100%',
+            margin: '50px'
+        },
+        paper: {
+            padding: theme.spacing(2),
+            margin: '10px',
+            textAlign: 'center',
+            color:  'white',//theme.palette.text.secondary,
+            background: colors.mixtapeRows,
+        },
+        card: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+            color:  theme.palette.text.secondary,
+            background: theme.palette.success.light,
+        },
+        box: {
+            padding: theme.spacing(2),
+            textAlign: 'center',
+            color:  theme.palette.info.contrastText,
+            background: blueGrey[800],
+            minWidth: '50%',
+            height: '100%'
+        }
+    }));
+
+    const classes = useStyles();
+
     const { user, setUser } = useContext(UserContext);
 
     return (
-        <div style={{color: 'white', left: 0}}>
-            <div style={{margin: 'auto', width: '90%'}}>
-            <Grid container
-              direction="column"
-              justify="center"
-              alignItems="center"
-              >
-                <Grid item xs={2} style={{background:colors.mixtapeRows}}>
-                Bread
-                </Grid>
-                <Grid item xs={2} style={{background:colors.mixtapeRows}}>
-                Yogurt
-                </Grid>
-            </Grid>
-            </div>
+        <div style={{color: 'white', left: 0, height: '100%'}}>
+            <Grid container spacing={1} className={classes.grid}>
+                <Box className={classes.box}> 
+                    <Typography variant="headline" component="h1">
+                        Popular Mixtapes This Week
+                    </Typography>
+                    <Grid item xs={12} md={12}>
+                        <Paper className={classes.paper}>
+                            <div display='inline-block'>
+                                <Typography variant="headline" component="h4" align='left'>Title</Typography>
+                                <Typography variant="headline" component="h4" align='center'>Collaborators</Typography>
+                                <Typography variant="headline" component="h4" align='right'>Favorites</Typography>
+                            </div>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Paper className={classes.paper}>
+                            Blocc 2
+                        </Paper>
+                    </Grid>
+                </Box>
 
+            </Grid>
             <br />
         </div>
     );
