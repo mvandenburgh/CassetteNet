0fc01db2c486d3e717462149cd35c164961f406c
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-10-27 12:07:32 -0400

    Made some links

--- client/src/components/Directory.jsx
+++ client/src/components/Directory.jsx
@@ -2,38 +2,38 @@ import React, { useContext, useState } from 'react';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import UserContext from '../contexts/UserContext';
-import HidePageFrameContext from '../contexts/HidePageFrameContext';
-import NavigateContext from '../contexts/NavigateContext';
-
 
 
 function Directory() {
     // const { setUser } = useContext(UserContext);
     // setUser({ username: 'Guest', isGuest: true, isLoggedIn: false });
-    const { navigate, setNavigate } = useContext(NavigateContext);
-    const { hidePF, setHidePF } = useContext(HidePageFrameContext);
     const history = useHistory();
 
     //history.push('/');
 
-    const goLogin = () => {  setNavigate({ navigate: false });
-                            setHidePF({ hidePF: true});
+    const goLogin = () => {  
                             history.push('/login');
                             };
 
     return (
         <div style={{position: 'relative'}}>
-            <Button onClick={ history.push('/start') } style={{margin: '1em', backgroundColor: 'red'}} fullWidth variant="contained">Start Page</Button>
-            {/* <Button onClick={ history.push('/login') } style={{margin: '1em', backgroundColor: 'blue'}} fullWidth variant="contained">Login Page</Button> */}
-            
-            <Button variant="outline-primary">Primary</Button>
-            <Button variant="outline-secondary">Secondary</Button>
-            <Button variant="outline-success">Success</Button>
-            <Button variant="outline-warning">Warning</Button>
-            <Button variant="outline-danger">Danger</Button>
-            <Button variant="outline-info">Info</Button>
-            <Button variant="outline-light">Light</Button>
-            <Button variant="outline-dark">Dark</Button>
+
+            <Button onClick={ () => history.push('/start') } variant="outlined" style={{margin: '1em'}}>Start Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/login') } variant="outlined" style={{margin: '1em'}}>Login Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/atmosphere') } variant="outlined" style={{margin: '1em'}}>Atmosphere Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/mixtape/:id') } variant="outlined" style={{margin: '1em'}}>View Mixtape Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/mymixtapes') } variant="outlined" style={{margin: '1em'}}>My Mixtapes Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/inbox') } variant="outlined" style={{margin: '1em'}}>Inbox Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/NotFound') } variant="outlined" style={{margin: '1em'}}>Not Found Page</Button>
+            <br/>
+            <Button onClick={ () => history.push('/SignUp') } variant="outlined" style={{margin: '1em'}}>Sign Up Page</Button>
+
             
         </div>
     );
