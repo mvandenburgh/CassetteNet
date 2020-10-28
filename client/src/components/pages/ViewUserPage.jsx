e128570c714ac8680087f283bf8615bc240f1cfe
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-10-28 18:39:51 -0400

    Added the skeleton for the rows of mixtapes

--- client/src/components/pages/ViewUserPage.jsx
+++ client/src/components/pages/ViewUserPage.jsx
@@ -1,19 +1,71 @@
 import React, { useContext, useState } from 'react';
-import { Button, Grid, Typography, makeStyles, IconButton } from '@material-ui/core';
+import { AppBar, Box, Button, Grid, Tab, Tabs, Typography, makeStyles, IconButton } from '@material-ui/core';
 import {
     alpha,
     ThemeProvider,
     withStyles,
     createMuiTheme,
   } from '@material-ui/core/styles';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 import { getUsername } from '../../utils/api';
 import { users } from '../../testData/users.json'
 import pfp from '../../images/bottle_pfp.jpg';
 import ReactRoundedImage from "react-rounded-image";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { useHistory } from 'react-router-dom';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+
+//   const MakeRows = ({mixtapes}) => (
+//     <>
+//       {mixtapes.map(mixtape => (
+//         <div className="mixtape" key={mixtape}>{mixtape}</div>
+//       ))}
+//     </>
+//   ); 
+
+    const MakeRows = ({mixtapes}) => (
+    <>
+      {mixtapes.map(mixtape => (
+        <Box margin="5px" padding="10px" bgcolor="#ff9800" className="mixtape" key={mixtape}>{mixtape}</Box>
+      ))}
+    </>
+    ); 
+
+  var favorites = users[1].favoritedMixtapes;
+  
 
 function ViewUserPage(props) {
       const useStyles = makeStyles((theme) => ({
@@ -32,6 +84,12 @@ function ViewUserPage(props) {
 
     const history = useHistory();
     const goBack = () => { history.push('/') }
+
+    const [value, setValue] = React.useState(0);
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
 
     return (
         <div  style={{ color: 'white', left:0 }}>
@@ -55,19 +113,33 @@ function ViewUserPage(props) {
                     </div>
                 </div>
                 <div style={{width: '75%', backgroundColor: 'pink'}}>
-                <Tabs>
-                    <TabList>
-                        <Tab>Created Mixtapes</Tab>
-                        <Tab>Favorited Mixtapes</Tab>
-                    </TabList>
-                
-                    <TabPanel>
-                        <h2>Any content 1</h2>
+                    <AppBar position="static">
+                        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                            <Tab label="Created Mixtapes" {...a11yProps(0)} />
+                            <Tab label="Favorite Mixtapes" {...a11yProps(1)} />
+                        </Tabs>
+                    </AppBar>
+                    <TabPanel value={value} index={0}>
+                        <Box display="flex" flexDirection="row" bgcolor="#2196f3" width="99%">
+                            <Box width="33%" textAlign="center" boxShadow={3} borderRight={1} bgcolor="#2196f3">
+                                Name
+                            </Box>
+                            <Box width="33%" textAlign="center" boxShadow={3} borderRight={1} bgcolor="#2196f3">
+                                Collaborators
+                            </Box>
+                            <Box width="33%" textAlign="center" boxShadow={3} bgcolor="#2196f3">
+                                Favorites
+                            </Box>
+                        </Box>
+                        <Box margin="5px" bgcolor="#9c27b0"> 
+                            
+                                <MakeRows mixtapes={favorites} />
+                            
+                        </Box>
                     </TabPanel>
-                    <TabPanel>
-                        <h2>Any content 2</h2>
+                    <TabPanel value={value} index={1}>
+                        Item Two
                     </TabPanel>
-                </Tabs>
                 </div>
             </div>
         </div>
