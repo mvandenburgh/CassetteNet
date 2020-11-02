ff1cfa5ee7323d8a4c51b1ec2ad37525dbf4eb75
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2020-11-02 15:13:56 -0500

    make first user to sign up an admin (#72)

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 router.post('/signup', async (req, res) => {
     const { username, password, email } = req.body;
-    User.register(new User( {username, email, verified: false }), password, (err, user) => {
+
+    // this new user should be an admin if there are 0 users currently
+    const userCount = await User.estimatedDocumentCount();
+
+    User.register(new User({ username, email, verified: false, admin: userCount === 0 }), password, (err, user) => {
         if (err) res.send(err); // TODO: error handling
 
         passport.authenticate('local')(req, res, () => res.send(user));
