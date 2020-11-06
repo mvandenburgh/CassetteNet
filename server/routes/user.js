4dad8b8f97443680eff471989fe73c373d9581a1
unknown <hiimprat@gmail.com>
2020-11-06 01:28:17 -0500

    [WIP] Account Verification

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,17 +15,20 @@ router.post('/signup', async (req, res) => {
 
     User.register(new User({ username, email, verified: false, admin: userCount === 0 }), password, (err, user) => {
         if (err) res.send(err); // TODO: error handling
-
-        passport.authenticate('local')(req, res, () => res.send(user));
-    });
-        var token =  crypto.randomBytes(16).toString('hex');
+        else{
+            var token =  crypto.randomBytes(16).toString('hex');
         //TODO: add token to database
-            var transporter = nodemailer.createTransport({ service: 'Gmail', auth: { user: "Your Gmail ID", pass: "Gmail Pass"} });
+            var transporter = nodemailer.createTransport({ service: 'Gmail', auth: { user: "hiimprat@gmail.com", pass: "Guruguru1212"} });
             var mailOptions = { from: 'no-reply@cassettenet.com', to: 'hiimprat@gmail.com', subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + token + '.\n' };
         transporter.sendMail(mailOptions, function (err) {
         if (err) { return res.status(500).send({ msg: err.message }); }
         res.status(200).send('A verification email has been sent to ' + user.email + '.');
     });
+        passport.authenticate('local')(req, res, () => res.send(user));
+        }
+        
+    });
+        
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
