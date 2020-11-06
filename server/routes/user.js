48f770d0f40ca57dd6a296fa9e51dd7e3ac46ec6
unknown <hiimprat@gmail.com>
2020-11-06 00:23:10 -0500

    [WIP] Account Verification test

--- server/routes/user.js
+++ server/routes/user.js
@@ -19,8 +19,8 @@ router.post('/signup', async (req, res) => {
 
         token.save(function(err){
             if (err) res.send(err);
-            var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD } });
-            var mailOptions = { from: 'no-reply@cassettenet.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
+            var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: username, pass: password } });
+            var mailOptions = { from: 'no-reply@cassettenet.com', to: 'hiimprat@gmail.com', subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
             transporter.sendMail(mailOptions, function (err) {
                 if (err) { return res.status(500).send({ msg: err.message }); }
                 res.status(200).send('A verification email has been sent to ' + user.email + '.');
