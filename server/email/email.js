606c29f0cc10a9c4374bd6a0ed6a5d53ed62a4cb
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-07 18:43:24 -0500

    prevent mailgun error when env var isn't defined

--- server/email/email.js
+++ server/email/email.js
@@ -5,13 +5,19 @@ const ejs = require('ejs');
 
 const verificationEmailTemplate = fs.readFileSync(path.join(__dirname, 'verificationEmailTemplate.ejs'), 'utf-8');
 
-const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
+const MAILGUN_API_KEY =  process.env.MAILGUN_API_KEY;
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
+
+let mg;
+if (process.env.NODE_ENV === 'production') {
+    mg = mailgun({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN });
+}
 
 const sendVerificationEmail = async (recipient, verificationToken) => {
     const verficationUrl = `https://cassettenet.netlify.app/verify/${verificationToken}`;
     const emailBody = ejs.render(verificationEmailTemplate, { verficationUrl });
     const data = {
-        from: `verify@${process.env.MAILGUN_DOMAIN}`,
+        from: `verify@${MAILGUN_DOMAIN}`,
         to: recipient,
         subject: 'Verify your CassetteNet account',
         html: emailBody,
