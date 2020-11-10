e5658210938c0053ad3553080e3b786696ee29cc
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-11-10 00:15:07 -0500

    Add jsTPS Context Provider to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import ViewUserPage from './components/pages/ViewUserPage';
 import UserContext from './contexts/UserContext';
 import CurrentSongContext from './contexts/CurrentSongContext';
 import PlayingSongContext from './contexts/PlayingSongContext';
+import JSTPSContext from './contexts/JSTPSContext';
+import { jsTPS } from './utils/jsTPS'
 import Directory from './components/Directory';
 import ListeningRoomPage from './components/pages/ListeningRoomPage';
 import ViewProfilePage from './components/pages/ViewProfilePage';
@@ -53,47 +55,52 @@ function App() {
 
 
   const [playing, setPlaying] = useState(false);
+  
+  var my_tps = new jsTPS();
+  const [tps, setTps] = useState(my_tps);
 
   return (
     <div className="App">
-      <UserContext.Provider value={{user, setUser}}>
-        <CurrentSongContext.Provider value={{currentSong, setCurrentSong}}>
-          <PlayingSongContext.Provider value={{playing, setPlaying}}>
-            <BrowserRouter>
-                <PageFrame invisible={!user.isLoggedIn} />
-                  <div style={{ marginBottom: '105px', position: 'absolute', left: 8*9, height: 'calc(100vh - 8*9)', width: 'calc(100vw - 73px)'}}>
-                    <Switch>
-                      <Route exact path="/" component={user.isLoggedIn ? DashboardPage : StartPage} />
-                      <Route exact path="/directory" component={Directory} /> {/* TODO: remove? */}
-                      <Route exact path="/login" component={LoginPage} />
-                      <Route exact path="/atmosphere" component={AtmospherePage} />
-                      <Route exact path="/mixtape/:id" component={ViewMixtapePage} />
-                      <Route exact path="/mymixtapes" component={MyMixtapesPage} />
-                      <Route exact path="/favoritedmixtapes" component={FavoritedMixtapesPage} />
-                      <Route exact path="/viewuser" component={ViewUserPage} />
-                      <Route exact path="/followedusers" component={FollowedUsersPage}/>
-                      <Route exact path="/anonymousmixtapes" component={AnonymousMixtapesPage}/>
-                      <Route exact path="/inbox" component={InboxPage} />
-                      <Route exact path="/NotFound" component={NotFoundPage}/>
-                      <Route exact path="/SignUp" component={SignUpPage}/>
-                      <Route exact path="/ChangePassword" component={ChangePasswordPage}/>
-                      <Route exact path="/verify/:token" component={VerifyAccountPage} />
-                      <Route exact path="/Admin">
-                        {user.admin ? <AdminPage /> : <Redirect to="/" />}
-                      </Route>
-                      <Route exact path="/me" component={ViewProfilePage}/>
-                      <Route exact path="/user/:id" component={ViewUserPage}/>
-                      <Route exact path="/SearchResultsPage" component={SearchResultsPage}/>
-                      <Route exact path="/listeningroom" component={ListeningRoomPage} /> {/* temporary route for listening room testing */}
-                      <Route path="/*">
-                        <Redirect to="/" />
-                      </Route>
-                    </Switch>
-                  </div>
-            </BrowserRouter>
-          </PlayingSongContext.Provider>
-        </CurrentSongContext.Provider>
-      </UserContext.Provider>
+      <JSTPSContext.Provider value={{tps, setTps}}>
+        <UserContext.Provider value={{user, setUser}}>
+          <CurrentSongContext.Provider value={{currentSong, setCurrentSong}}>
+            <PlayingSongContext.Provider value={{playing, setPlaying}}>
+              <BrowserRouter>
+                  <PageFrame invisible={!user.isLoggedIn} />
+                    <div style={{ marginBottom: '105px', position: 'absolute', left: 8*9, height: 'calc(100vh - 8*9)', width: 'calc(100vw - 73px)'}}>
+                      <Switch>
+                        <Route exact path="/" component={user.isLoggedIn ? DashboardPage : StartPage} />
+                        <Route exact path="/directory" component={Directory} /> {/* TODO: remove? */}
+                        <Route exact path="/login" component={LoginPage} />
+                        <Route exact path="/atmosphere" component={AtmospherePage} />
+                        <Route exact path="/mixtape/:id" component={ViewMixtapePage} />
+                        <Route exact path="/mymixtapes" component={MyMixtapesPage} />
+                        <Route exact path="/favoritedmixtapes" component={FavoritedMixtapesPage} />
+                        <Route exact path="/viewuser" component={ViewUserPage} />
+                        <Route exact path="/followedusers" component={FollowedUsersPage}/>
+                        <Route exact path="/anonymousmixtapes" component={AnonymousMixtapesPage}/>
+                        <Route exact path="/inbox" component={InboxPage} />
+                        <Route exact path="/NotFound" component={NotFoundPage}/>
+                        <Route exact path="/SignUp" component={SignUpPage}/>
+                        <Route exact path="/ChangePassword" component={ChangePasswordPage}/>
+                        <Route exact path="/verify/:token" component={VerifyAccountPage} />
+                        <Route exact path="/Admin">
+                          {user.admin ? <AdminPage /> : <Redirect to="/" />}
+                        </Route>
+                        <Route exact path="/me" component={ViewProfilePage}/>
+                        <Route exact path="/user/:id" component={ViewUserPage}/>
+                        <Route exact path="/SearchResultsPage" component={SearchResultsPage}/>
+                        <Route exact path="/listeningroom" component={ListeningRoomPage} /> {/* temporary route for listening room testing */}
+                        <Route path="/*">
+                          <Redirect to="/" />
+                        </Route>
+                      </Switch>
+                    </div>
+              </BrowserRouter>
+            </PlayingSongContext.Provider>
+          </CurrentSongContext.Provider>
+        </UserContext.Provider>
+      </JSTPSContext.Provider>
     </div>
   );
 }
