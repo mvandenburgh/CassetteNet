8e66ce787372df78244ad73e82911f339d217169
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-09 19:12:56 -0500

    fix another music player-related crash

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -157,7 +157,7 @@ function Player(props) {
       setShuffle(!shuffleState);
     }
 
-    if (playerRef?.current && playerRef && playerRef.current) {
+    if (playerRef?.current && currentSong) {
         currentSong.duration = playerRef.current.getDuration();
         setCurrentSong(currentSong);
     }
