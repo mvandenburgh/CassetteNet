559e61780f305ade320229467170ec09fab6bddf
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-17 14:09:49 -0500

    Refactor followed user list into reusable component

--- client/src/components/pages/FollowedUsersPage.jsx
+++ client/src/components/pages/FollowedUsersPage.jsx
@@ -11,14 +11,11 @@ import {
   DialogTitle,
   DialogActions
 } from '@material-ui/core';
-import { blueGrey, indigo, lightBlue } from '@material-ui/core/colors'
 import { Autocomplete } from '@material-ui/lab';
 import { ArrowBack as ArrowBackIcon } from '@material-ui/icons';
-import ReactRoundedImage from "react-rounded-image";
 import { useHistory } from 'react-router-dom';
 import UserContext from '../../contexts/UserContext';
-import { getUserProfilePictureUrl } from '../../utils/api';
-import FollowUserButton from '../FollowUserButton';
+import UserList from '../UserList';
 
 function FollowedUsersPage(props) {
   const [open, setOpen] = useState(false);
@@ -31,14 +28,9 @@ function FollowedUsersPage(props) {
     setOpen(false);
   };
 
-  const [highlightedRow, setHighlightedRow] = useState(null);
-
   const colors = {
-    followedUserRowColor: blueGrey[900],
-    followedUserRowMouseOverColor: blueGrey[500],
-    unfollowButtonColor: lightBlue[500],
     searchButtonColor: lightBlue[700],
-  }
+}
 
   const suggestedUsers = [
     { name: 'DDrizzy123' },
@@ -48,49 +40,6 @@ function FollowedUsersPage(props) {
     { name: 'BobMarley' },
     { name: 'CoolName' },
   ];
-
-  const FollowedUserRows = ({ followedUsers, history }) => (
-    <>
-      {followedUsers.map((user, index) => (
-        <div onMouseEnter={() => setHighlightedRow(index)} onMouseLeave={() => setHighlightedRow(null)}>
-          <Box
-            style={{
-              margin: "5px",
-              padding: "10px",
-              backgroundColor: index === highlightedRow ? colors.followedUserRowMouseOverColor : colors.followedUserRowColor,
-              cursor: 'pointer',
-              display: "flex",
-              flexDirection: "row",
-              borderRadius: 6,
-              fontSize: 12,
-            }}
-            boxShadow={3}
-            onClick={() => history.push(`/user/${user._id}`)}
-            >
-            <Box style={{ width: "33%", display: 'flex', flexDirection: 'row', marginLeft: '15px' }}>
-              <ReactRoundedImage image={getUserProfilePictureUrl(user._id)} roundedSize="1" imageWidth="100" imageHeight="100" />
-              <br />
-              <Box style={{ fontSize: '15pt', width: "50%", display: 'flex', justifyContent: "left", marginLeft: '15px' }}> {user.username}#{user.uniqueId} </Box>
-            </Box>
-
-            <Box style={{ fontSize: '12pt', marginLeft: '50px', width: "33%", display: 'flex', flexDirection: 'column' }}>
-              Last activity: {user.updatedAt}
-              <br />
-              <br />
-                  User since: {user.createdAt}
-              <br />
-              <br />
-                  Followers: {user.followers}
-            </Box>
-            <Box style={{ width: "25%", display: 'flex', justifyContent: "left" }}>
-              <br />
-                <FollowUserButton id={user._id} backgroundColor={colors.unfollowButtonColor} />
-            </Box>
-          </Box>
-        </div>
-      ))}
-    </>
-  );
 
   const { user } = useContext(UserContext);
 
@@ -141,7 +90,7 @@ function FollowedUsersPage(props) {
         <Button onClick={handleClickOpen} variant="contained" boxShadow={3} style={{ margin: 'auto', backgroundColor: colors.searchButtonColor }}> Search for User</Button>
       </Box>
       <div style={{ width: "70%", margin: 'auto' }}>
-        <FollowedUserRows followedUsers={user.followedUsers} history={history} />
+        <UserList users={user.followedUsers} />
       </div>
     </div>
   );
