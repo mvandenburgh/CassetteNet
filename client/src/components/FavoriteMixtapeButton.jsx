cc55121417bd5d24c2ecc7f579001b8ed9728530
Michael VanDenburgh <michael.vandenburgh@stonybrook.edu>
2020-11-23 15:07:09 -0500

    throttle favorite mixtape button clicks

--- client/src/components/FavoriteMixtapeButton.jsx
+++ client/src/components/FavoriteMixtapeButton.jsx
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Favorite as FavoriteIcon } from '@material-ui/icons';
 import { favoriteMixtape, unfavoriteMixtape } from '../utils/api';
 import UserContext from '../contexts/UserContext';
 
 function FavoriteMixtapeButton(props) {
     const { user, setUser } = useContext(UserContext);
+    const [disabled, setDisabled] = useState(false);
     const favoriteButtonClickHandler = async () => {
+        if (disabled) return;
+        setDisabled(true);
         let favoritedMixtapes;
         if (user.favoritedMixtapes.includes(props.id)) {
             favoritedMixtapes = await unfavoriteMixtape(props.id);
@@ -15,9 +18,10 @@ function FavoriteMixtapeButton(props) {
         const newUser = { ...user};
         newUser.favoritedMixtapes = favoritedMixtapes;
         setUser(newUser);
+        setDisabled(false);
     }
     return (
-        <FavoriteIcon onClick={() => favoriteButtonClickHandler()} style={{color: user?.favoritedMixtapes?.includes(props.id) ? 'red' : 'black', cursor:'pointer'}} /> 
+        <FavoriteIcon onClick={() => favoriteButtonClickHandler()} style={{color: disabled ? 'grey' : user?.favoritedMixtapes?.includes(props.id) ? 'red' : 'black', cursor:'pointer'}} /> 
     )
 }
 
