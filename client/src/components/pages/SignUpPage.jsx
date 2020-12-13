49706dca9240ca51251c8d4e90bfc46cdc46109e
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-12-13 14:33:43 -0500

    Restrict username and password at sign up

--- client/src/components/pages/SignUpPage.jsx
+++ client/src/components/pages/SignUpPage.jsx
@@ -14,12 +14,41 @@ function SignUpPage(props) {
   const handlePassword = (e) => setPassword(e.target.value);
   const handleEmail = (e) => setEmail(e.target.value);
 
+  const validateUsername = () => {
+    return (username.length >= 4 && username.length <= 12);
+  }
+
+  var passwordValidator = require('password-validator');
+  var schema = new passwordValidator();
+  schema
+  .is().min(8)                                    // Minimum length 8
+  .is().max(100)                                  // Maximum length 100
+  .has().uppercase()                              // Must have uppercase letters
+  .has().lowercase()                              // Must have lowercase letters
+  .has().digits(1)                                // Must have at least 1 digits
+  .has().not().spaces()                           // Should not have spaces
+  .has().symbols(1)
+  //.has().oneOf(['!', '@', '#', '$', '%', '&', '*']);
+
+  const validatePassword = () => {
+    console.log(schema.validate(password, { list: true }));
+    return (schema.validate(password));
+  }
+
   const submit = () => {
-    if(username.length < 4) {
-      setOpen(true);
+    if(!validateUsername() ) {
+      setInvalidUsernameDialogOpen(true);
+    }
+    else { 
+      setInvalidUsernameDialogOpen(false);
+    }
+    if(!validatePassword()) {
+      setInvalidPasswordDialogOpen(true);
     }
     else {
-      setOpen(false);
+      setInvalidPasswordDialogOpen(false);
+    }
+    if (validateUsername() && validatePassword()){
       userSignup(email, username, password)
       .then(() => alert('Sign up successful!'))
       .catch(err => alert(err));
@@ -38,11 +67,18 @@ function SignUpPage(props) {
       width: '100px',
       marginLeft: '20px',
       marginRight: '20px',
-    }
+    },
+    root: {
+      borderRadius: 3,
+      border: 0,
+      height: 48,
+      bottom: 100,
+    },
   }));
   const classes = useStyles();
 
-  const [open, setOpen] = useState(false);
+  const [invalidUsernameDialogOpen, setInvalidUsernameDialogOpen] = useState(false);
+  const [invalidPasswordDialogOpen, setInvalidPasswordDialogOpen] = useState(false);
 
   const history = useHistory();
   const goBack = () => history.goBack();
@@ -55,6 +91,37 @@ function SignUpPage(props) {
         <ArrowBackIcon />
       </IconButton>
 
+      <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+        open={invalidUsernameDialogOpen}
+        autoHideDuration={4000}
+        // onClose={handleClose}
+        message="Username must be between 4 and 12 characters."
+        // action={
+        //     <React.Fragment>
+        //         <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+        //             <CloseIcon fontSize="small" />
+        //         </IconButton>
+        //     </React.Fragment>
+        // }
+      />
+      <Snackbar
+        classes={{
+         root: classes.root,
+        }}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={invalidPasswordDialogOpen}
+        autoHideDuration={4000}
+        // onClose={handleClose}
+        message="Password must be at least 8 characters in length, with no spaces 
+        and at least 1 of the following: Uppercase, lowercase, number, special character (!, @, #, $, %, &, *)."
+      />
 
       <Typography align="center" variant="h3">
         <br />
@@ -64,24 +131,7 @@ function SignUpPage(props) {
       </Typography>
       <Grid container spacing={1} alignItems="center" direction="column">
         <Grid container spacing={1} alignItems="center" direction="row">
-        <Snackbar
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                open={open}
-                autoHideDuration={4000}
-                // onClose={handleClose}
-                message="Username must be between 4 and 12 characters."
-                // action={
-                //     <React.Fragment>
-                //         <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
-                //             <CloseIcon fontSize="small" />
-                //         </IconButton>
-                //     </React.Fragment>
-                // }
-              >
-                </Snackbar>
+
             <Grid item>
 
               <TextField
